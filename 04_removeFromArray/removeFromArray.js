6fcbd3b056d2c2be7e02bf8c33bee8d6ec9e1c02
Drumus <miguelibanezpico@gmail.com>
2022-03-11 21:37:01 +0100

    Ex04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    
+    //let args = Array.from(arguments);
+    let numArg = arguments.length;
+    let arrLen = arr.length;
+    
+    
+    //start from i = 1 because the array will be index 0.
+    for (let i = 1; i < numArg; i++)
+    {
+        let index = arr.indexOf(arguments[i]);
+        if( index != -1) 
+        { 
+            arr.splice(index, 1);   
+        }
+    }
+    return(arr);
 };
 
 // Do not edit below this line
