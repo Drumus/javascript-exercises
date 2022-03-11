ce9f59b47bbed002bb5789dbdd9fb8f65f5ca392
Drumus <miguelibanezpico@gmail.com>
2022-03-11 20:50:01 +0100

    Ex03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
-};
+/* /* const reverseString = function(str) {
+    let revstr = "";
+    let splitStr = str.split("");
+    let reverseArray = splitStr.reverse();
+    let joinArray = reverseArray.join("");
+    revstr = joinArray;
+    return(revstr);
+}; */
+const reverseString = function(str) {
+    let revStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        revStr += str[i];
+    }
+    return revStr;
+}
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
