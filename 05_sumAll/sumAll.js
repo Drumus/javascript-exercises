1674e307b603f2687dda563a0f179276776d6169
Drumus <miguelibanezpico@gmail.com>
2022-03-11 21:51:23 +0100

    Ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (x < 0 || y < 0 || typeof(x) != "number" || typeof(y) != "number")
+    {
+        return("ERROR");
+    }
+    if (x <= y) 
+    { 
+        for (let i = x; i <= y; i++)
+        {
+            sum += i; 
+        }
+    }
+    else 
+    {
+        for (let i = y; i <= x; i++)
+        {
+            sum += i; 
+        }
+    }
+    return(sum);
 };
 
 // Do not edit below this line
