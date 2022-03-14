abc0a97ddfc64d59a9aa12557fd40172056c65e3
Drumus <miguelibanezpico@gmail.com>
2022-03-14 20:03:03 +0100

    Ex08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+  arr.forEach(num => {
+    result += num;
+  });
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let result = 1;
+  arr.forEach(num => {
+    result *= num;
+  });
+  return result;
+};
+
+const power = function(x, y) {
+	result = 1;
+  for (i = 0; i < y; i++) {
+    result *= x;
+  }
+  return result;
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+	result = 1;
+  if (x > 0) {
+    for (i = 1; i <= x; i++) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
