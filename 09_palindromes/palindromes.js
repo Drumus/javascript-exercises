e6c1736011324ef43264beec3a89fc1f042a0e67
Drumus <miguelibanezpico@gmail.com>
2022-03-14 20:59:29 +0100

    Ex09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let revStr = '';
+    newStr = str.toLowerCase().replace(/[^a-z]/g,"");
+    
+    for (i = 0; i < newStr.length; i ++) {
+
+        revStr = revStr + newStr[newStr.length -1 - i];
+        //console.log(i);
+    }
+
+    //tests
+    /* console.log(str);
+    console.log(revStr);
+    console.log(str.length);
+    console.log(newStr.length);
+    console.log(newStr);
+    console.log(revStr.length); */
+
+    return (newStr === revStr) ? true : false;
 
 };
 
