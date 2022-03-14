cd97d0923208fff142a4ab5103ffbddcf95c3b63
Drumus <miguelibanezpico@gmail.com>
2022-03-14 21:21:53 +0100

    Ex10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0) {
+        return "OOPS";
+    }
+    fibo = [1, 1];
+    for (let i = 2; i < 50; i++) {
+        fibo[i]= Number(fibo[i - 2]) + Number(fibo[i - 1]);
+        //console.log(i);
+    } 
+    //console.log(fibo);
+    return Number(fibo[index - 1]);
 };
 
 // Do not edit below this line
