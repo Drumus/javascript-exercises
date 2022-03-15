a037dacbaf2f64f8436c6114296937ce360cd6fe
Drumus <miguelibanezpico@gmail.com>
2022-03-15 11:40:06 +0100

    Ex12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldestName = people.reduce((oldest,currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+    return oldestName;
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear(); //return current year
+    }
+    return death - birth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
